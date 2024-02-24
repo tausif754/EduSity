2bd2d077ca499b245263fb2087063bf3134ca6c3
Tausif Ahmad <tausifahmad754@gmail.com>
2024-02-24 20:52:49 +0530

    Program section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Programs from "./components/Programs/Programs";
+import Title from "./components/Title/Title";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title subTitle="OUR PROGRAMS" title="What we offer" />
+        <Programs />
+      </div>
     </div>
   );
 };
